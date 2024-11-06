e0ab242ada4aa617bcea7f4993dbee84bd3ca84b
Nate Valley <46508762+NateValley@users.noreply.github.com>
2024-11-06 22:52:48 +0000

    L16 Alert Button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-// todo
+import "./style.css";
+
+const APP_NAME = "D3: Geocoin Carrier";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+document.title = APP_NAME;
+app.innerHTML = APP_NAME;
+
+const alertButton = document.createElement("button");
+alertButton.innerHTML = "Click me!";
+app.append(alertButton);
+
+alertButton.addEventListener("click", () => {
+  alert("you make a big click!");
+});
