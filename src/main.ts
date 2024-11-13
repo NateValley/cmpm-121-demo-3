374222efec536dd15eb9fa0efc7a14d619ce8d59
Nate Valley <46508762+NateValley@users.noreply.github.com>
2024-11-13 02:05:00 +0000

    D3.a Completed!

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,123 @@
 import "./style.css";
 
-const APP_NAME = "D3: Geocoin Carrier";
-const app = document.querySelector<HTMLDivElement>("#app")!;
+// @deno-types="npm:@types/leaflet@^1.9.14"
+import leaflet from "leaflet";
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
 
-const alertButton = document.createElement("button");
-alertButton.innerHTML = "Click me!";
-app.append(alertButton);
+// Fix missing marker images
+import "./leafletWorkaround.ts";
 
-alertButton.addEventListener("click", () => {
-  alert("you made a big click!");
+// Deterministic random number generator
+import luck from "./luck.ts";
+
+// Location of our classroom (as identified on Google Maps)
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+// Tunable gameplay parameters
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+// Create the map (element with id "map" is defined in index.html)
+const map = leaflet.map(document.getElementById("map")!, {
+  center: OAKES_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
+
+// Populate the map with a background tile layer
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+// Add a marker to represent the player
+const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+playerMarker.bindTooltip("That's you!");
+playerMarker.addTo(map);
+
+// Display the player's coins
+let playerCoins = 0;
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!; // element `statusPanel` is defined in index.html
+statusPanel.innerHTML = "No coins yet...";
+
+// Add caches to the map by cell numbers
+function spawnCache(i: number, j: number) {
+  // Convert cell numbers into lat/lng bounds
+  const origin = OAKES_CLASSROOM;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  // Add a rectangle to the map to represent the cache
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+
+  // Handle interactions with the cache
+  let tempValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+
+  rect.bindPopup(() => {
+    // Each cache has a random point value, mutable by the player
+    let coinValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+
+    if (tempValue != coinValue) {
+      coinValue = tempValue;
+    }
+
+    // The popup offers a description and button
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+                <div>There is a cache here at "${i},${j}". It has value <span id="value">${coinValue}</span>.</div>
+                <button id="grab">grab coin</button><button id="donate">donate</button>`;
+
+    // Clicking the button decrements the cache's value and increments the player's coins
+    popupDiv
+      .querySelector<HTMLButtonElement>("#grab")!
+      .addEventListener("click", () => {
+        if (coinValue > 0) {
+          coinValue--;
+          playerCoins++;
+        }
+        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = coinValue
+          .toString();
+        statusPanel.innerHTML = `${playerCoins} coins accumulated`;
+        tempValue = coinValue;
+      });
+
+    popupDiv
+      .querySelector<HTMLButtonElement>("#donate")!
+      .addEventListener("click", () => {
+        if (playerCoins > 0) {
+          coinValue++;
+          playerCoins--;
+        }
+        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = coinValue
+          .toString();
+        statusPanel.innerHTML = `${playerCoins} coins accumulated`;
+        tempValue = coinValue;
+      });
+
+    return popupDiv;
+  });
+}
+
+// Look around the player's neighborhood for caches to spawn
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    // If location i,j is lucky enough, spawn a cache!
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(i, j);
+    }
+  }
+}
